14f0fb92e8d340358a8810fe9f8fbc8f50b776db
sahnoune aya <sahnouneaya15@gmail.com>
2024-04-26 18:20:46 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { route } = require('./routers/students.route')
+const userRoute = require('./routers/user.route')
+
+const app= express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use((req,res,next)=>{
+    res.setHeader('Access Control Allow Origin','*')
+    res.setHeader('Access Control Request Method','*')
+    res.setHeader('Access Control Allow Headers','*')
+    next()
+})
+
+
+
+
+app.use('/',route)
+app.use('/',userRoute)
+
+app.listen(3000, ()=>{
+    console.log('port is listining')
+})
